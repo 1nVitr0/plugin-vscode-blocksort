b5ab0b2edad4b059946993fde1a0a90b25330135
Aram Becker <becker.aram@gmail.com>
2021-04-02 12:57:21 +0200

    test: add more tests for cpp-like languages

--- src/test/fixtures/expand.ts
+++ src/test/fixtures/expand.ts
@@ -7,4 +7,9 @@ export const expandTests: ExpandTest[] = [
     ranges: [new Range(3, 9, 3, 9)],
     targetRanges: [new Range(3, 0, 16, 20)],
   },
+  {
+    file: 'expand.cpp.fixture',
+    ranges: [new Range(2, 6, 2, 6), new Range(19, 4, 19, 4), new Range(0, 1, 0, 1)],
+    targetRanges: [new Range(2, 0, 4, 13), new Range(19, 0, 21, 11), new Range(0, 0, 32, 2)],
+  },
 ];
