20e23c7fd279fbdd5ce237d937947908c0b7aa0e
Aram Becker <becker.aram@gmail.com>
2021-12-14 22:18:21 +0100

    fix: make code lenses optional

--- src/contribute/formattingActions.ts
+++ src/contribute/formattingActions.ts
@@ -1,14 +1,20 @@
 import { languages } from "vscode";
 import BlockSortActionProvider from "../providers/BlockSortActionProvider";
 import FormattingProvider from "../providers/FormattingProvider";
+import ConfigurationProvider from "../providers/ConfigurationProvider";
 
 export default function contributeFormattingActions() {
   const formattingProvider = new FormattingProvider();
   const codeActionsProvider = new BlockSortActionProvider(formattingProvider);
-  return [
+
+  const disposables = [
     languages.registerDocumentFormattingEditProvider("*", formattingProvider),
     languages.registerDocumentRangeFormattingEditProvider("*", formattingProvider),
-    languages.registerCodeLensProvider("*", codeActionsProvider),
     languages.registerCodeActionsProvider("*", codeActionsProvider),
   ];
+  if (ConfigurationProvider.getEnableCodeLens()) {
+    disposables.push(languages.registerCodeLensProvider("*", codeActionsProvider));
+  }
+
+  return disposables;
 }
