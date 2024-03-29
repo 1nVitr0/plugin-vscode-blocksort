91b505c69fea16b3eb26f41ed5fb0dd14090a1c1
Aram Becker <becker.aram@gmail.com>
2024-03-29 15:17:11 +0100

    test: additional tests for customSortOrder

--- src/test/fixtures/custom.ts
+++ src/test/fixtures/custom.ts
@@ -1,10 +1,17 @@
 import { Range } from "vscode";
-import { CompareTest } from "../suite/types";
+import { CompareTest, CustomSortTest } from "../suite/types";
 
-export const customSortTests: CompareTest[] = [
+export const customSortTests: CustomSortTest[] = [
   {
     file: "custom.plaintext.fixture",
     compareFile: "custom.plaintext.expect",
     ranges: [new Range(0, 0, 5, 4)],
+    collatorOptions: { customSortOrder: "abcdefghijklmnopqrstuvwxyz:#" },
+  },
+  {
+    file: "custom.plaintext.fixture",
+    compareFile: "custom.plaintext.expect",
+    ranges: [new Range(7, 0, 21, 1)],
+    collatorOptions: { customSortOrder: '"-_' },
   },
 ];
